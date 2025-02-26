37a86b4dd7885010c0a06eed1c02a4f7188a2584
HerrVonUnruh <Kruck_offiziell@proton.me>
2025-02-26 14:38:30 +0100

    Push bc working on laptop

--- Source/HighAndHigher/LobbyGameMode.cpp
+++ Source/HighAndHigher/LobbyGameMode.cpp
@@ -11,6 +11,13 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
 	if (NumberOfPlayers == 2)
 	{
+		UWorld* World = GetWorld();
 
+		if (World)
+		{
+
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/BlasterMap?listen"));
+		}
 	}
 }
